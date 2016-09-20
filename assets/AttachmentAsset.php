8f3406e58f171339d61ed393dffc447ec72987ea
xiewulong <xiewulong@vip.qq.com>
2016-09-20 20:44:29 +0800

    attachment

--- assets/AttachmentAsset.php
+++ assets/AttachmentAsset.php
@@ -5,25 +5,27 @@
  * https://github.com/xiewulong/yii2-attachment
  * https://raw.githubusercontent.com/xiewulong/yii2-attachment/master/LICENSE
  * create: 2016/8/27
- * update: 2016/8/27
+ * update: 2016/9/20
  * since: 0.0.1
  */
 
 namespace yii\attachment\assets;
 
 use Yii;
-use yii\web\AssetBundle;
+use yii\components\AssetBundle;
 
 class AttachmentAsset extends AssetBundle {
 
 	public $sourcePath = '@yii/attachment/dist';
 
-	public $js = [
-		'js/Attachment.js',
-	];
-
 	public $depends = [
 		'yii\xui\JqueryAsset',
 	];
+
+	public function init() {
+		parent::init();
+
+		$this->js[] = 'js/Attachment' . $this->minimal . '.js';
+	}
 
 }
--- dist/js/Attachment.js
+++ dist/js/Attachment.js
@@ -1,3 +1,9 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 /*!
  * Attachment
  * xiewulong <xiewulong@vip.qq.com>
@@ -5,9 +11,13 @@
  * version: 0.0.1
  */
 
-(function($, window, document, undefined) {
-	var Attachment	= function(input, fn, prop) {
-			prop = prop || {};
+(function ($, window, document, undefined) {
+	var Attachment = function () {
+		function Attachment(input, fn) {
+			var prop = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
+			_classCallCheck(this, Attachment);
+
 			this.input = input;
 			this._name = this.input.name;
 			this.$input = $(input);
@@ -16,62 +26,78 @@
 			this.multiple = prop.multiple || this.$input.attr('data-attachment-multiple');
 			this.csrf = {
 				param: prop.csrf_param || this.$input.attr('data-csrf-param') || '_csrf',
-				token: prop.csrf_token || this.$input.attr('data-csrf-token'),
+				token: prop.csrf_token || this.$input.attr('data-csrf-token')
 			};
 			this.pre = prop.pre || 'Attachment';
-			this.before = prop.before || function() {};
+			this.before = prop.before || function () {};
 			this.fn = fn;
 
 			this.input.value && this.action && this.fn && this.upload();
-		};
-
-	Attachment.prototype = {
-		upload: function() {
-			var _this = this;
-			this.before && this.before.call(this.input);
-			this.setName();
-			this.createElements();
-			window[_this.name] = function(d) {_this.callback(d);};
-			this.$form.submit();
-		},
-		createElements: function() {
-			this.$iframe = $('<iframe style="display:none;" name="' + this.name + '"></iframe>').appendTo('body');
-			this.$form = $('<form style="display:none;" action="' + this.action + '" target="' + this.name + '" method="post" enctype="multipart/form-data"></form>').appendTo('body');
-			this.multiple && this.$form.append('<input type="hidden" name="multiple" value="' + this.multiple + '" />');
-			this.csrf.token && this.$form.append('<input type="hidden" name="' + this.csrf.param + '" value="' + this.csrf.token + '" />');
-			this.$form.append('<input type="hidden" name="name" value="' + this.name + '" />');
-			this.$form.append(this.input);
-		},
-		setName: function() {
-			var random = (+ new Date()).toString() + Math.floor(Math.random() * 999 + 100).toString();
-			this.name = this.pre + random;
-			this.input.name = this.name + (this.multiple ? '[]' : '');
-		},
-		callback: function(d) {
-			this.input.name = this._name;
-			this.input.value = '';
-			this.$parent.append(this.input);
-			this.$form.remove();
-			this.$iframe.remove();
-			this.fn.call(this.input, d);
 		}
-	}
 
-	$(document).on('change', '[data-attachment-upload]', function() {
-		new Attachment(this, function(d) {
+		_createClass(Attachment, [{
+			key: 'upload',
+			value: function upload() {
+				var _this = this;
+
+				this.before && this.before.call(this.input);
+
+				this.setName();
+				this.createElements();
+
+				window[this.name] = function (d) {
+					_this.callback(d);
+				};
+
+				this.$form.submit();
+			}
+		}, {
+			key: 'createElements',
+			value: function createElements() {
+				this.$iframe = $('<iframe style="display:none;" name="' + this.name + '"></iframe>').appendTo('body');
+				this.$form = $('<form style="display:none;" action="' + this.action + '" target="' + this.name + '" method="post" enctype="multipart/form-data"></form>').appendTo('body');
+				this.multiple && this.$form.append('<input type="hidden" name="multiple" value="' + this.multiple + '" />');
+				this.csrf.token && this.$form.append('<input type="hidden" name="' + this.csrf.param + '" value="' + this.csrf.token + '" />');
+				this.$form.append('<input type="hidden" name="name" value="' + this.name + '" />');
+				this.$form.append(this.input);
+			}
+		}, {
+			key: 'setName',
+			value: function setName() {
+				var random = (+new Date()).toString() + Math.floor(Math.random() * 999 + 100).toString();
+
+				this.name = this.pre + random;
+				this.input.name = this.name + (this.multiple ? '[]' : '');
+			}
+		}, {
+			key: 'callback',
+			value: function callback(d) {
+				this.input.name = this._name;
+				this.input.value = '';
+				this.$parent.append(this.input);
+				this.$form.remove();
+				this.$iframe.remove();
+				this.fn.call(this.input, d);
+			}
+		}]);
+
+		return Attachment;
+	}();
+
+	$(document).on('change', '[data-attachment-upload]', function () {
+		new Attachment(this, function (d) {
 			$(this).trigger('uploaded.attachment.file', d);
 		}, {
-			'before': function() {
+			'before': function before() {
 				$(this).trigger('upload.attachment.file');
 			}
 		});
 	});
 
-	$.fn.attachment = function(fn, prop) {
-		return this.on('change', function() {
-			new fileupload(this, fn, prop);
+	$.fn.attachment = function (fn, prop) {
+		return this.on('change', function () {
+			new Attachment(this, fn, prop);
 		});
 	};
-
-	window.Attachment = Attachment;
 })(jQuery, window, document);
+//# sourceMappingURL=Attachment.js.map
